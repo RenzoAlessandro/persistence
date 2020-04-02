09d5eb592cec69134e418104fd3d74d5553d7c9f
Roger P Aranibar <aranibar.roger@gmail.com>
2020-04-02 00:29:33 -0500

    Use extension of node, add main samples

--- source/DirectedGraph.hpp
+++ source/DirectedGraph.hpp
@@ -5,69 +5,56 @@
 #include <cstddef>
 #include <iostream>
 
-// #include "PartialDirectedGraph.hpp"
-// class PartialDirectedGraph;
-
 namespace ADE {
 
 template <typename Type>
-class DirectedGraph {
+class Node {
  public:
   typedef Type data_type;
 
-  // friend class PartialDirectedGraph;
+  Node(data_type data, std::size_t const& out_ptrs_size)
+      : data_(new data_type(data)),
+        out_ptrs_size_(out_ptrs_size),
+        forward_(new Node<Type>*[out_ptrs_size]()) {}
+
+  virtual ~Node() {}
 
   /**
-   * Node
-   **/
-  struct Node {
-    Node() : data_(nullptr), forward_(nullptr) {}
-    Node(data_type data, std::size_t const& out_ptrs_size)
-        : data_(new data_type(data)),
-          out_ptrs_size_(out_ptrs_size),
-          forward_(new Node* [out_ptrs_size]()) {
+   *  \brief Access specified element
+   *
+   *  Returns a reference to the element at specified location position. If id
+   * is not within the range of the container an exception of type
+   * std::out_of_range is thrown. If no node exist at the location
+   * std::logic_error is thrown.
+   *
+   */
+  Node& operator[](std::size_t id) const {
+    if (out_ptrs_size_ < id) {
+      throw std::out_of_range("Index out of node edges range.");
     }
-
-    ~Node() {
-      for (std::size_t i = 0; i < out_ptrs_size_; ++i) {
-        delete[] forward_[i];
-      }
+    if (!forward_[id]) {
+      throw std::logic_error("Access to null reference.");
     }
+    return *forward_[id];
+  }
 
-    /**
-     *  \brief Access specified element
-     *
-     *  Returns a reference to the element at specified location position. If id
-     * is not within the range of the container an exception of type
-     * std::out_of_range is thrown. If no node exist at the location
-     * std::logic_error is thrown.
-     *
-     */
-    Node& operator[](std::size_t id) const {
-      if (out_ptrs_size_ < id) {
-        throw std::out_of_range("Index out of node edges range.");
-      }
-      if (!forward_[id]) {
-        throw std::logic_error("Access to null reference.");
-      }
-      return *forward_[id];
-    }
+  data_type* data_;
+  Node** forward_;
+  std::size_t out_ptrs_size_;
+};
 
-    data_type* data_;
-    Node** forward_;
-    std::size_t out_ptrs_size_;
-  };
+template <typename Type, typename Node = Node<Type>>
+class DirectedGraph {
+ public:
+  typedef Type data_type;
 
   DirectedGraph(data_type const data, std::size_t const& out_ptrs_size)
       : root_ptr_(new Node(data, out_ptrs_size)),
         out_ptrs_size_(out_ptrs_size) {}
 
-  explicit DirectedGraph(std::size_t const& out_ptrs_size)
-      : out_ptrs_size_(out_ptrs_size) {}
-
   virtual ~DirectedGraph() {}
 
-  Node& get_root() { return *root_ptr_; }
+  Node* get_root_ptr() { return root_ptr_; }
 
   /**
    *  \brief Inserts element
@@ -83,11 +70,11 @@ class DirectedGraph {
     if (out_ptrs_size_ < position) {
       throw std::out_of_range("Insert position out of node edges range.");
     }
-    Node* next_node_ptr = u->forward_[position];
+    Node* next_node_ptr = dynamic_cast<Node*>(u->forward_[position]);
     Node* new_node = new Node(data, out_ptrs_size_);
     new_node->forward_[position] = next_node_ptr;
     u->forward_[position] = new_node;
-    return u->forward_[position];
+    return dynamic_cast<Node*>(u->forward_[position]);
   }
 
   /**
@@ -104,7 +91,7 @@ class DirectedGraph {
     u->forward_[position] = v;
   }
 
- protected:
+ private:
   Node* root_ptr_;
   std::size_t out_ptrs_size_;
 };
--- source/PartialDirectedGraph.hpp
+++ source/PartialDirectedGraph.hpp
@@ -2,30 +2,64 @@
 #ifndef SOURCE_PARTIALDIRECTEDGRAPH_HPP_
 #define SOURCE_PARTIALDIRECTEDGRAPH_HPP_
 
+#include <utility>
+
 #include "DirectedGraph.hpp"
 
 namespace ADE {
 namespace Persistence {
 
 template <typename Type>
-class PartialDirectedGraph : public DirectedGraph<Type> {
+class PartialNode : public Node<Type> {
+ public:
+  typedef Type data_type;
+  PartialNode(data_type data, std::size_t const& out_ptrs_size)
+      : Node<Type>(data, out_ptrs_size) {}
+
+  PartialNode& operator[](
+      std::pair<std::size_t, unsigned int> id_version) const {
+    return *dynamic_cast<PartialNode*>(
+        &(Node<Type>::operator[](id_version.first)));
+  }
+};
+
+template <typename Type, typename Node = PartialNode<Type>>
+class PartialDirectedGraph : public DirectedGraph<Type, Node> {
  public:
   typedef Type data_type;
 
-  struct PartialNode: public DirectedGraph<Type>::Node {
-    PartialNode(data_type data, std::size_t const& out_ptrs_size)
-        : DirectedGraph<Type>::Node(data, out_ptrs_size) {
-    }
-  };
-
   PartialDirectedGraph(data_type const data, std::size_t const& out_ptrs_size,
                        std::size_t const& in_ptrs_size)
-      : DirectedGraph<Type>(out_ptrs_size), in_ptrs_size_(in_ptrs_size) {
-    DirectedGraph<Type>::root_ptr_ = new PartialNode(data, out_ptrs_size);
+      : DirectedGraph<Type, Node>(data, out_ptrs_size),
+        in_ptrs_size_(in_ptrs_size),
+        current_version(0) {}
+
+  Node* get_root_ptr(unsigned int version) {
+    return DirectedGraph<Type, Node>::get_root_ptr();
+  }
+
+  Node* insert_vertex(data_type const data, Node* u, std::size_t position) {
+    ++current_version;
+    return insert_vertex(data, u, position, current_version);
+  }
+
+  void add_edge(Node* u, Node* v, std::size_t position) {
+    ++current_version;
+    return add_edge(u, v, position, current_version);
   }
 
  private:
+  Node* insert_vertex(data_type const data, Node* u, std::size_t position,
+                      unsigned int version) {
+    return DirectedGraph<Type, Node>::insert_vertex(data, u, position);
+  }
+
+  void add_edge(Node* u, Node* v, std::size_t position, unsigned int version) {
+    return DirectedGraph<Type, Node>::add_edge(u, v, position);
+  }
+
   std::size_t in_ptrs_size_;
+  unsigned int current_version;
 };
 
 }  // namespace Persistence
