5d5e68234025caf45f63d81f19a65784ac87c1be
Roger P Aranibar <aranibar.roger@gmail.com>
2020-04-08 20:55:41 -0500

    Change to PartialGraph

--- source/main.cpp
+++ source/main.cpp
@@ -11,22 +11,23 @@
 int main() {
   // Create a DirectedGraph, a maximum of 5 edged each node and the root with
   // the starting valuee of 1.
-  ADE::DirectedGraph<int> my_graph(1, 5);
+  ADE::Persistence::PartialDirectedGraph<int> my_graph(1, 5, 3);
 
   // We can obtain the root and print their value.
-  ADE::Node<int>* root_ptr = my_graph.get_root_ptr();
+  ADE::Persistence::PartialNode<int>* root_ptr = my_graph.get_root_ptr(1);
   std::cout << "Root Value: " << *root_ptr->data_ << std::endl;
 
   // Also, we can insert a new vertex passing the new Node value, a Node
   // reference and the index of the pointer used from the reference node to
   // the
   // inserted node.
-  ADE::Node<int>* inserted_node_ptr = my_graph.insert_vertex(2, root_ptr, 0);
+  ADE::Persistence::PartialNode<int>* inserted_node_ptr =
+      my_graph.insert_vertex(2, root_ptr, 0);
 
   // Likewise, the method to insert a new vertex, returns a reference of the
   // new
   // vertex inserted
-  ADE::Node<int>* other_new_node_ptr =
+  ADE::Persistence::PartialNode<int>* other_new_node_ptr =
       my_graph.insert_vertex(3, inserted_node_ptr, 1);
 
   // To add an edge between two vertex, we call the function using both Nodes
@@ -36,16 +37,21 @@ int main() {
 
   // We traverse the Graph from a reference node (using any Node pointer
   // provided by the previous methods), with the operator []
-  ADE::Node<int> retrived_node = (*root_ptr)[0][1];
+  ADE::Persistence::PartialNode<int> retrived_node =
+      (*root_ptr)[std::pair<std::size_t, unsigned int>(0, 1)]
+                 [std::pair<std::size_t, unsigned int>(1, 1)];
   std::cout << "Node Value: " << *retrived_node.data_ << std::endl;
 
   // It is also possible to insertt between two Nodes.
-  ADE::Node<int>* between_node_ptr =
+  ADE::Persistence::PartialNode<int>* between_node_ptr =
       my_graph.insert_vertex(5, inserted_node_ptr, 1);
-  retrived_node = (*root_ptr)[0][1];
+  retrived_node = (*root_ptr)[std::pair<std::size_t, unsigned int>(0, 1)]
+                             [std::pair<std::size_t, unsigned int>(1, 1)];
   std::cout << "Between Value: " << *retrived_node.data_ << std::endl;
 
-  retrived_node = (*root_ptr)[0][1][1];
+  retrived_node = (*root_ptr)[std::pair<std::size_t, unsigned int>(0, 1)]
+                             [std::pair<std::size_t, unsigned int>(1, 1)]
+                             [std::pair<std::size_t, unsigned int>(1, 1)];
   std::cout << "Next Value: " << *retrived_node.data_ << std::endl;
 
   return 0;
